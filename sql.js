c8bc7d5b66fb14983325c05bf32d9c3aa03bcc26
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2020-03-08 18:19:42 +0100

    hello from heroku db

--- sql.js
+++ sql.js
@@ -5,20 +5,20 @@ const { Client } = require('pg');
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-    });
+});
 
 client.connect();
 
 /*
-CREATE TABLE merchants (shop_name VARCHAR(255) PRIMARY KEY, offline_token VARCHAR(255), online_token VARCHAR(225))
-select * from information_schema.columns WHERE table_name = 'merchants'
-insert into merchants values ('thisismyshop', 'offline12345678900987654321', 'online09876543211234567890')
+CREATE TABLE merchant_entries (shop_name VARCHAR(255) PRIMARY KEY, offline_token VARCHAR(255), online_token VARCHAR(225))
+select * from information_schema.columns WHERE table_name = 'merchant_entries'
+insert into merchant_entries values ('thisismyshop', 'offline12345678900987654321', 'online09876543211234567890')
 select * from merchants
 */
 
 module.exports = {
     Select1:  function(query) {
-        client.query("select * from merchant_entries", (err, res) => {
+        client.query("insert into merchant_entries values ('hellofromheroku', 'offline12345678900987654321', 'online09876543211234567890')", (err, res) => {
             if (err) throw err;
             for (let row of res.rows) {
                 console.log(JSON.stringify(row));
