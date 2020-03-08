9b58249251fc1e345eaacd2522ae7b91f6017af9
bernardo-rodriguez <bernardoarodriguez@gmail.com>
2020-03-08 23:25:46 +0100

    use pools instead of individual client

--- sql.js
+++ sql.js
@@ -1,11 +1,26 @@
 require("openssl")
 
-const { Client } = require('pg');
+const { Pool, Client } = require('pg');
 
-const client = new Client({
+const pool = new Pool()
+
+pool.connect((err, client, release) => {
+    if (err) {
+      return console.error('Error acquiring client', err.stack)
+    }
+    client.query('select * from merchant_entries;', (err, result) => {
+      release()
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+      console.log(result.rows)
+    })
+  })
+
+/*const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-});
+});*/
 
 // const client = new Client({
 //     user: 'bernardorodriguez',
@@ -16,24 +31,35 @@ const client = new Client({
 //  });
  
 
-client.connect();
+//client.connect();
 
 /*
 CREATE TABLE merchant_entries (shop_name VARCHAR(255) PRIMARY KEY, offline_token VARCHAR(255), online_token VARCHAR(225))
 select * from information_schema.columns WHERE table_name = 'merchant_entries'
 insert into merchant_entries values ('thisismyshop', 'offline12345678900987654321', 'online09876543211234567890')
-select * from merchants
+select * from merchant_entries
 */
 
 
 module.exports = {
     merchantAuth:  function(shop, accessToken) {
-        client.query("insert into merchant_entries (shop_name, offline_token) values ($1, $2) ON CONFLICT (shop_name) DO UPDATE SET offline_token = $2", [shop, accessToken], (err, res) => {
+        pool.connect((err, client, release) => {
+            if (err) {
+              return console.error('Error acquiring client', err.stack)
+            }
+            client.query('select * from merchant_entries;', (err, result) => {
+              release()
+              if (err) {
+                return console.error('Error executing query', err.stack)
+              }
+              console.log(result.rows)
+            })
+          })
+        /*client.query("insert into merchant_entries (shop_name, offline_token) values ($1, $2) ON CONFLICT (shop_name) DO UPDATE SET offline_token = $2", [shop, accessToken], (err, res) => {
             if (err) throw err;
             for (let row of res.rows) {
                 console.log(JSON.stringify(row));
             }
-            client.end();
-        });
+        });*/
     }
 }
